84adecd76880a564589633277d3bdb8adda06633
FY Kuo <teresa99339@gmail.com>
2022-08-13 12:05:57 +0800

    完成找密碼功能

--- front/forgot.php
+++ front/forgot.php
@@ -1,1 +1,40 @@
-forgot
+<fieldset class="w-70 aut" style="padding: 20px;">
+    <legend>忘記密碼</legend>
+
+    <table class="aut w-80">
+        <tr>
+            <td >
+                請輸入信箱以查詢密碼
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input type="text" name="email" id="email" style="width: 100%;">
+            </td>
+        </tr>
+        <tr>
+            <td class="showText">
+
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input type="button" value="尋找" onclick="findPw()">
+            </td>
+        </tr>
+    </table>
+</fieldset>
+
+<script>
+    function findPw(){
+        let email = $('#email').val();
+
+        $.get('./api/forgot.php',{email},(res)=>{
+            
+
+            $('.showText').text(res)
+
+
+        })
+    }
+</script>
